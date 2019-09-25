d1d33241e0bb8fc9df5bb9e72d8d069a3f834afa
user <user@CarshMac.local>
2019-09-25 16:14:57 -0700

    Modularization

--- script.py
+++ script.py
@@ -8,7 +8,66 @@ import copy
 # from config import *
 import config
 
-if __name__ == '__main__':
+def stop_times_to_df(file, sparkSession):
+	df = sparkSession.read.csv(file, header=True, sep=',')
+	df = df.withColumn('stop_sequence', df['stop_sequence'].cast('int'))
+	return df
+
+def vehicle_pos_to_df(file, sparkSession):
+	cols_to_drop = ('vehicle_id', 'vehicle_label', 'vehicle_license_plate', \
+		'trip_start_time', 'bearing', 'speed', 'stop_status', 'occupancy_status', \
+		'congestion_level', 'progress', 'block_assigned', 'dist_along_route', 'dist_from_stop')
+
+	df = sparkSession.read.csv(file, header=True, sep=',').cache()
+	df = df.select([column for column in df.columns if column not in cols_to_drop])
+	df = df.withColumn('timestamp', df['timestamp'].cast('timestamp'))
+	return df
+
+def pos_stop_initial_join(stops_df, pos_df):
+	common_cols = ['trip_id', 'stop_id']
+	return pos_df.join(stops_df, common_cols) \
+		.groupby('trip_id', 'stop_id', 'stop_sequence') \
+		.agg(f.min('timestamp').alias('min_timestamp')) \
+		.orderBy(*common_cols)
+
+def time_delta(df, window):
+	return df['min_timestamp'].cast('long') - f.lag(df['min_timestamp']).over(window)
+
+def df_with_window_time_diff(df, window):
+	
+	seq_delta = df['stop_sequence'] - f.lag(df['stop_sequence']).over(window)
+	time_delta = time_delta(df, window)
+	time_per_stop = seq_delta / time_delta
+
+	return df.select(df['trip_id'], \
+		df['stop_id'], \
+		df['route_id'], \
+		df['stop_sequence'], \
+		df['min_timestamp'], \
+		time_delta.alias('time_delta'), \
+		seq_delta.alias('seq_delta'), \
+		time_per_stop.alias('time/stop'))
+
+def join_window_df_with_stops_df(windowdf, stopsdf):
+	common_cols = ['trip_id', 'stop_id', 'stop_sequence']
+	return stopsdf.join(windowdf, common_cols, 'left_outer')
+
+def df_with_window_backfill(df, window1, window2):
+	last_stop_id = f.lag(df['stop_id']).over(window1)
+	
+	df = df.withColumn('prev_stop', last_stop_id)
+	
+	time_per_stop_backfilled = f.last(df['time/stop'], ignorenulls=True).over(window2))
+
+	df = df.withColumn('filled_ts', time_per_stop_backfilled)
+
+	df = df.withColumn('day', f.dayofweek(df['min_timestamp']))
+	df = df.withColumn('hour', f.hour(df['min_timestamp']))
+
+	return df
+
+
+if  __name__ == '__main__':
 
         # Creates SparkSession
         scSpark = SparkSession.builder.appName('Stop to Stop').getOrCreate()
@@ -25,77 +84,27 @@ if __name__ == '__main__':
         if len(sys.argv) > 1:
                 stops_file = sys.argv[1]
                 data_file = sys.argv[2]
-
-        stop_times = scSpark.read.csv(stops_file, header=True, sep=',')
-	stop_times = stop_times.withColumn('stop_sequence', stop_times['stop_sequence'].cast('double'))
-
-        print('-'*12)
-        print('-'*12)
-        print('STOP TIMES')
-        stop_times.show()
-
-        assert stop_times.schema.names is not None
 	
-	print('='*20)
+	stops_df = stop_times_to_df(stops_file, scSpark)
+
+        assert stops_df.schema.names is not None
+	
 	print('STOP TIMES SCHEMA')
-	print(stop_times.schema.names)
-	print('='*20)
+	print(stops_df.schema.names)
 
         # Read csv, put into dataframe. Store in memory.
-        sdfData = scSpark.read.csv(data_file, header=True, sep=',').cache()
+        updates_df = vehicle_pos_to_df(data_file, scSpark)
 
-        cols_to_drop = ('vehicle_id', 'vehicle_label', 'vehicle_license_plate', 'trip_start_time', \
-                'bearing', 'speed', 'stop_status', 'occupancy_status', 'congestion_level', \
-                'progress', 'block_assigned', 'dist_along_route', 'dist_from_stop')
-
-        sdfData = sdfData.select([column for column in sdfData.columns if column not in cols_to_drop])
-	
-	sdfData = sdfData.withColumn('timestamp', sdfData['timestamp'].cast('timestamp'))
-	
-        # sdfData = sdfData.drop(sdfData.vehicle_id).collect()
-
-
-	print('='*20)
         print('UPDATES SCHEMA')
-        print(sdfData.schema.names)
+        print(updates_df.schema.names)
         print('='*20)
 
         print('-'*12)
         print('UPDATES')
-        sdfData.show()
-
-        condition = ['trip_id', 'stop_id']
-        
-        # joined_df = sdfData.join(stop_times, on=condition, how='inner')
+        updates_df.show()
 	
-       	minned_df = sdfData.join(stop_times, ['trip_id', 'stop_id']) \
-			.groupby('trip_id', 'stop_id', 'stop_sequence') \
-			.agg(f.min('timestamp').alias('min_timestamp')) \
-			.orderBy('trip_id', 'stop_id') 
-			# .select(f.col("min(timestamp)").alias("min_timestamp"))
-	# assert len(joined_df.head(1)) != 0
-
-        # print('JOINED_DF')
-        # print('-'*12)
-
-	# properties = {
-          #  "user": "postgres",
-           # "password": "postgres"
-       # }
-
-        #joined_df.show()
-
-	# joined_df.write.jdbc(url=config.url, \
-          #      table="sts", mode='overwrite', properties=properties)	
-
-
-
-        # print(joined_df.schema.names)
-        # print('-'*12)
-
-        # minned_df = joined_df.groupby('trip_id', 'stop_id', 'stop_sequence') \
-          #      .agg(f.min('timestamp').alias('min_timestamp')).orderBy('trip_id', 'stop_id')
-
+       	minned_df = stop_pos_initial_join(stops_df, updates_df)
+	
 	assert len(minned_df.head(1)) != 0
 
 	print('MINNED DF')
@@ -106,74 +115,28 @@ if __name__ == '__main__':
 	print('Printed schema')
 	minned_df.printSchema()
 	
-	# In order to use a physical window function, we sort by trip_id and timestamp
-
-	# columns = ['trip_id', 'min_timestamp']
-	# minned_sorted = minned_df.orderBy(columns, ascending=[0, 1])
-
-	# assert len(minned_sorted.head(1)) != 0
-
-	# print('MINNED SORTED')
-	# print('='*12)
-	# minned_sorted.show()
-
-	window_spec = Window \
+	window_look_back_one = Window \
 		.partitionBy('trip_id') \
 		.orderBy('min_timestamp')
 	
-	time_delta = minned_df.min_timestamp.cast('long') - f.lag(minned_df.min_timestamp).over(window_spec).cast('long')
+	window_df = df_with_window_time_diff(minned_df, window_look_back_one)
 
-	seq_delta = minned_df.stop_sequence - f.lag(minned_df.stop_sequence).over(window_spec)
-
-	time_per_stop = time_delta / seq_delta
-
-	print('MINNED WINDOW')
-	print('='*12)
-	print('='*12)
-
-	min_win = minned_df.select(minned_df['trip_id'], \
-		minned_df['stop_id'], \
-		minned_df['stop_sequence'], \
-		minned_df['min_timestamp'], \
-		time_delta.alias('time_delta'), \
-		seq_delta.alias('seq_delta'), \
-		time_per_stop.alias('time/stop'))
-
-	min_win.show()
-
-	stop_times_join = stop_times.join(min_win, ['trip_id', 'stop_id', 'stop_sequence'], 'left_outer')
-	stop_times_join = stop_times_join.withColumn('prev_stop', f.lag(stop_times_join.stop_id).over(window_spec))
+	stop_times_joined_df = join_window_df_with_stops_df(window_df, stops_df)
 	
-	new_window = Window \
+	window_look_forward_many = Window \
 		.partitionBy('trip_id') \
 		.orderBy('min_timestamp') \
 		.rowsBetween(0, sys.maxsize)
 
 	# Thanks to John Paton for forward fill (altered here for backward fill).
 	# https://johnpaton.net/posts/forward-fill-spark/
-
-	filled = f.last(stop_times_join['time/stop'], ignorenulls=True).over(new_window)
-
-	stop_times_join = stop_times_join.withColumn('filled', filled)
-
-	stop_times_join.withColumn('day', f.dayofweek(stop_times_join['min_timestamp']))
 	
-	stop_times_join.withColumn('hour', f.hour(stop_times_join['min_timestamp']))
+	df_backfilled = df_with_window_backfill(stop_times_joined_df, window_look_back_one, window_look_forward_many)
 
-	print('JOIN')
-	stop_times_join.show()
-
-        # After that, map days of the week and time windows.
-
-        # ultimate_df.show()
-
-
-        properties = {
+	properties = {
             "user": "postgres",
             "password": "postgres"
         }
 	
-	
-
         stop_times_join.write.jdbc(url=config.url, \
                 table="data", mode='overwrite', properties=properties)
