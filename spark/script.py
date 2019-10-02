2ac9642bbce8a40f27c9191c852b311f0c4b04f8
user <user@CarshMac.local>
2019-10-02 13:49:10 -0700

    Better comments. Est schedule difference

--- spark/script.py
+++ spark/script.py
@@ -17,7 +17,17 @@ def stop_times_to_df(file, sparkSession, trips_df):
 	df = sparkSession.read.csv(file, header=True, sep=',')
 	df = df.join(trips_df, ['trip_id'])
 	df = df.withColumn('stop_sequence', df['stop_sequence'].cast('int'))
+	df = df.withColumn('stop_time', df['stop_time'].cast('timestamp'))
+
 	return df
+
+def add_schedule_difference(df, window):
+	"""
+	Calculates difference between row and previous schedule times. Add as column.
+	"""
+	time_diff = df['stop_time'].cast('long') - f.lag(df['stop_time']).over(window).cast('long')
+
+	df = df.withColumn('schedule_delta', time_diff)
 
 def vehicle_pos_to_df(file, sparkSession):
 	""" 
@@ -39,7 +49,7 @@ def pos_stop_initial_join(stops_df, pos_df):
 	"""
 	common_cols = ['trip_id', 'stop_id', 'route_id']
 	return pos_df.join(stops_df, common_cols) \
-		.groupby('route_id', 'trip_id', 'stop_id', 'stop_sequence') \
+		.groupby('route_id', 'trip_id', 'stop_id', 'stop_sequence', 'schedule_delta') \
 		.agg(f.min('timestamp').alias('min_timestamp')) \
 		.orderBy('trip_id', 'stop_id')
 
@@ -64,6 +74,7 @@ def df_with_window_time_diff(df, window):
 		df['route_id'], \
 		df['stop_sequence'], \
 		df['min_timestamp'], \
+		df['schedule_delta'], \
 		time.alias('time_delta'), \
 		seq_delta.alias('seq_delta'), \
 		time_per_stop.alias('time/stop'))
@@ -105,9 +116,10 @@ def df_with_window_backfill(df, window1, window2):
 	df = df.withColumn('filled_ts', f.first(df['time/stop'], ignorenulls=True).over(window2))
 	df = df.withColumn('day', f.first(f.dayofweek(df['min_timestamp']), ignorenulls=True).over(window2))
 	df = df.withColumn('hour', f.first(f.hour(df['min_timestamp']), ignorenulls=True).over(window2))
-	df = df.withColumn('week', f.first(f.weekofyear(df['min_timestamp']), ignorenulls=True).over(window2))	
+	df = df.withColumn('week', f.first(f.weekofyear(df['min_timestamp']), ignorenulls=True).over(window2))
+	df = df.withColumn('diff_schedule_real', df['filled_ts'].cast('long') - df['schedule_delta'])	
 	
-	columns_to_return = ['trip_id', 'stop_id', 'prev_stop', 'stop_sequence', 'route_id', 'filled_ts', 'day', 'hour', 'week']
+	columns_to_return = ['trip_id', 'stop_id', 'prev_stop', 'stop_sequence', 'route_id', 'filled_ts', 'day', 'hour', 'week', 'diff_schedule_real']
 	df = df.filter(df.filled_ts.isNotNull()) \
 		.filter((df.filled_ts > 0)) \
 		.filter((df.filled_ts != 6666.6666))
@@ -131,32 +143,48 @@ if  __name__ == '__main__':
 		data_file = sys.argv[2]
 		trips_file = sys.argv[3]
 
+	# Begin with a straightforward read from csv.	
 	trips_df = scSpark.read.csv(trips_file, header=True, sep=',')
+	
+	# Stop times require casting. Convert to DataFrame
 	stops_df = stop_times_to_df(stops_file, scSpark, trips_df)
 	assert stops_df.schema.names is not None
 
+	# Vehicle positions require dropping columns and casting. Convert to DataFrame.
 	updates_df = vehicle_pos_to_df(data_file, scSpark)
 
+	# Join positions updates DF with stops DF to determine stop sequence.
+	# Return rows with minimum timestamp per stop_sequence.
 	minned_df = pos_stop_initial_join(stops_df, updates_df)
 	
 	assert len(minned_df.head(1)) != 0
 	
+	# Save date in data as date for naming S3 file later.
 	date = minned_df.first().asDict()['min_timestamp']
 	
+	# In case certain stop sequences occur twice, order Window by timestamp.
 	window_look_back_one = Window \
 		.partitionBy('trip_id') \
 		.orderBy('min_timestamp')
 	
+	# Calculate time difference between row and row previous.
 	window_df = df_with_window_time_diff(minned_df, window_look_back_one)
 
+	# In order to perform spark.sql function, add table.
 	window_df.createOrReplaceTempView('window_filled')
-	stops_df.createOrReplaceTempView('stops')
-
-	stop_times_joined_df = join_window_df_with_stops_df_sql('stops', 'window_filled', sqlCon)	
 
 	window_look_back_one_order_seq = Window \
 		.partitionBy('trip_id') \
 		.orderBy('stop_sequence')
+
+	# Determine approximate stop-to-stop time for the trip schedule.
+	stops_df = add_schedule_difference(stops_df, window_look_back_one_order_seq)
+
+	# Add table to sql context.
+	stops_df.createOrReplaceTempView('stops')
+
+	# Determine which trip_ids are represented, then join tables.
+	stop_times_joined_df = join_window_df_with_stops_df_sql('stops', 'window_filled', sqlCon)	
 
 	window_look_forward_many = Window \
 		.partitionBy('trip_id') \
@@ -166,8 +194,9 @@ if  __name__ == '__main__':
 	# Thanks to John Paton for forward fill (altered here for backward fill).
 	# https://johnpaton.net/posts/forward-fill-spark/
 	
+	# Backfill time delta over nulls.
 	df_backfilled = df_with_window_backfill(stop_times_joined_df, window_look_back_one_order_seq, window_look_forward_many)
 	
 	datestring = date.strftime("%Y-%m-%d")
-
+	
 	df_backfilled.write.save('s3a://stoptostop/preprocessed/'+datestring+'-preprocessed.csv', formate='csv', header=True)	
